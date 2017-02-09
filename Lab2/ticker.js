3c91844d6e5fc40d11b7489b3d1e8d646f37c2cf
Peter <kopeterj@gmail.com>
2017-02-08 22:17:05 -0500

    Lab2 Submission

--- Lab2/ticker.js
+++ Lab2/ticker.js
@@ -1,65 +1,101 @@
 // Function to make request and run ticker
-function tick() {
-  var weatherJSON = $.getJSON( "TwitterTweets17.json", function( data ) {
+function tick(latitude, longitude) {
 
-    navigator.geolocation.getCurrentPosition(showLocation, errorHandler, options);
+  var apiString = " https://api.forecast.io/forecast/ab229f452201da190c58043082aff43f/"
+                              +latitude+","+longitude;
 
-    // Get List of weathers
-    var weatherCards = document.getElementsByClassName('weatherbox');
+$.ajax({
+  url: "https://api.forecast.io/forecast/ab229f452201da190c58043082aff43f/"+latitude+","+longitude,
+  dataType: "jsonp",
+  success: function (data) {
+        console.log(data);
+        // Get List of weathers
+        var weatherCards = document.getElementsByClassName('weatherbox');
 
-    // Cycle Weather Cardts
-    function tock() {
+        // Cycle Weather Cards
+        function tock() {
 
-        // Element Deletion
-        $(weatherCards[0]).slideUp(300);
-        setTimeout(function(){$(weatherCards[0]).remove();}, 300);
+            // Element Deletion
+            $(weatherCards[0]).slideUp(300);
+            setTimeout(function(){$(weatherCards[0]).remove();}, 300);
 
-        // Element Creation in DOM
-        var liclass = document.createElement('li');
-        liclass.setAttribute("class", "weatherbox");
-        liclass.setAttribute("id", data[0].id);
-        var aclass = document.createElement('a');
-        aclass.setAttribute("class", "animated bounceInLeft");
-        liclass.appendChild(aclass);
-        aclass.innerHTML = data[0].text;
+            // Element Creation in DOM
+            var liclass = document.createElement('li');
+            liclass.setAttribute("class", "weatherbox");
+            //liclass.setAttribute("id", data[0].id);
+            var aclass = document.createElement('a');
+            aclass.setAttribute("class", "animated bounceInLeft");
+            liclass.appendChild(aclass);
+            var topDate = new Date(data.hourly.data[0].time *1000);
 
-        // Cycle JSON Data
-        data.push(data.shift());
 
-        // Add Element to DOM
-        $(tlist).append(liclass);
-    }
+            addString = topDate.getMonth() + 1 + "/" +
+                        topDate.getDate() + " ";
+            addString += formatAMPM(topDate);
+            myTemp = Math.round(data.hourly.data[0].temperature);
+            addString += " " + myTemp + "&#x2109";
+            addString += " " + data.hourly.data[0].summary;
+            addString += " <i class=\"wi wi-forecast-io-"+data.hourly.data[0].icon+"\"></i> "
+            aclass.innerHTML = addString;
 
-    // Initiailze Weather Card
-    // for (var p = 0; p < 5; ++p) {
-    //   tock();
-    // }
+            // Cycle JSON Data
+            data.hourly.data.push(data.hourly.data.shift());
 
-    // Constantly run ticker functions
-    setInterval(tock, 3000);
+            // Add Element to DOM
+            $(tlist).append(liclass);
+        }
 
-    // Get Data on Click
-    $("ul").on("click", ".weatherbox", function() {
-      var flag = false;
-      console.log(this.id);
-      for (var i = 0; i < data.length; i++) {
-          if (data[i].id == this.id) {
-              flag = true;
-              break;
-          }
-      }
+        // Constantly run ticker functions
+        setInterval(tock, 8000);
 
-      // Change weather info if match found
-      if (flag == true){
-        document.getElementById("portrait").src = data[i].user.profile_image_url;
-        document.getElementById("username").innerHTML = data[i].user.screen_name;
-        document.getElementById("description").innerHTML = data[i].user.description;
-        document.getElementById("text").innerHTML = data[i].text;
-      }
-    });
+        // Initiailze First 5 Weather Hourly Updates
+        for (var p = 0; p < 8; ++p) {
+          tock();
+        }
 
-  });
-};
 
-// Run Ticker
-tick();
+        // Print out current weather
+        document.getElementById("portrait").innerHTML = "<i class=\"wi wi-forecast-io-"+data.currently.icon+"\"></i>";
+        document.getElementById("temperature").innerHTML = Math.round(data.currently.temperature, 0) + "&#x2109";
+        document.getElementById("description").innerHTML = data.currently.summary;
+
+        // Parse & Print Time
+        var myDate = new Date( data.currently.time *1000);
+        dateString1 = myDate.toDateString();
+        dateString2 = formatAMPM(myDate);
+        document.getElementById("time1").innerHTML = dateString1;
+        document.getElementById("time2").innerHTML = dateString2;
+
+
+  }
+});
+}
+
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+
+
+   navigator.geolocation.getCurrentPosition(function( position ) {
+     var pos = {
+       lat: position.coords.latitude,
+       lng: position.coords.longitude
+     };
+     console.log(pos.lat);
+     console.log(pos.lng);
+
+     // Run Ticker
+     tick(pos.lat, pos.lng);
+   })
+}
+
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  hours = hours < 10 ? '0'+hours: hours;
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
---
+++ mantisbt-2.1.0/config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = 'mysqlPassword';
+
+$g_default_timezone       = 'America/New_York';
+
+$g_crypto_master_salt     = 'yaFOV/EU4AG/1o5gGLOU5v9hf7QsAYxMcCVMY17oxa0=';
