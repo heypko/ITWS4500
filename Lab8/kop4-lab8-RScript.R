11083c170f9e0d567ac70002410484c625055e95
Peter <kop4@rpi.edu>
2017-04-17 17:38:26 -0400

    Added labels :)

--- Lab8/kop4-lab8-RScript.R
+++ Lab8/kop4-lab8-RScript.R
@@ -12,29 +12,28 @@ edges <- edges[order(edges$listing_id, edges$review_id),]
 # Create graph
 colnames(edges[5]) <- "weight"
 rownames(edges) <- NULL
-net <- graph_from_data_frame(d=edges, vertices=nodes, directed=F) 
-
-# Plot graph
-plot(net, edge.arrow.size=.4,vertex.label=NA)
+net <- graph_from_data_frame(d=edges, vertices=nodes, directed=F)
 
 # Change node colors
-V(net)$color <- ifelse(V(net)$host_acceptance_rate > 90, "green", 
-                       ifelse(V(net)$host_acceptance_rate > 70, "gold", 
+V(net)$color <- ifelse(V(net)$host_acceptance_rate > 90, "green",
+                       ifelse(V(net)$host_acceptance_rate > 70, "gold",
                               ifelse(V(net)$host_acceptance_rate > 0, "red", "lightblue")))
 
 # Set node size based on audience size:
-#V(net)$size <- ifelse(V(net)$host_listings_count, V(net)$host_listings_count*1, 5)
-V(net)$size <-ifelse(!is.na(V(net)$accommodates), V(net)$accommodates*10, 5)
+V(net)$size <-ifelse(!is.na(V(net)$accommodates), V(net)$accommodates*6, 5)
 
 V(net)$size <- V(net)$size * .8
 
 #Set edge width to small
 E(net)$arrow.size <- .2
 
+
+
 # Plot Graph
-plot(net, vertex.label=V(net)$price, layout = cl,vertex.label=NA)
+plot(net, vertex.label=V(net)$review_scores_rating, layout = cl,vertex.label=NA, main="Airbnb Listings and Reviews", edge.curved=.1)
+
+text(x=-1.5, y=1, "#:\t  0-100 (Overall Rating)\nSize:  # of accommodations")
 
 # Create legend
-legend(x=-1.5, y=-1.1, c("<70% Acceptance",">90% Acceptance", ">70% Acceptance"), pch=21,
-       col="#777777", pt.bg=colrs, pt.cex=2, cex=.8, bty="n", ncol=1)
-
+legend(x=-1.5, y=-1.1, c(">90% Acceptance Rate",">70%", "<70%", "Review"), pch=21,
+       col=c("black"), pt.bg=c("green", "gold", "red", "lightblue"), pt.cex=2, cex=.8, bty="n", ncol=1)
